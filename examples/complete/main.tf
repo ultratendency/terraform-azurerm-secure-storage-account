84426ae4623d50cd070e4acb08f54a6a4ffcc561
Jan Hentschel <jan.hentschel@ultratendency.com>
2024-08-02 16:09:21 +0200

    feat(documentation): Update documentation to release 2.1.0 (#481)
    Update the documentation to reflect the next release 2.1.0.

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 
 module "terraform_state_storage_account" {
   source  = "ultratendency/secure-storage-account/azurerm"
-  version = "2.0.0"
+  version = "2.1.0"
 
   storage_account_name                                                   = "tstate"
   storage_account_resource_group_name                                    = "tstate"
