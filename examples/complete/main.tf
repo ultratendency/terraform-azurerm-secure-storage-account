c1fbc949a959d0dd437be7be16dc7b5d4b5d54af
Jan Hentschel <jan.hentschel@ultratendency.com>
2024-08-14 14:01:13 +0200

    fix(documentation): Update documentation to reference resource group
    Update the documentation and examples to clarify that the resource group
    used by the storage account needs to exist before using this module.

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -2,12 +2,16 @@ provider "azurerm" {
   features {}
 }
 
+data "azurerm_resource_group" "tstate" {
+  name = "tstate"
+}
+
 module "terraform_state_storage_account" {
   source  = "ultratendency/secure-storage-account/azurerm"
   version = "3.0.1"
 
   storage_account_name                                                   = "tstate"
-  storage_account_resource_group_name                                    = "tstate"
+  storage_account_resource_group_name                                    = data.azurerm_resource_group.tstate.name
   storage_account_location                                               = "westeurope"
   storage_account_account_tier                                           = "Premium"
   storage_account_account_replication_type                               = "GRS"
