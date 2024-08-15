beb365f8ffa5c43ef7030a210aff2eea4def1a32
Jan Hentschel <jan.hentschel@ultratendency.com>
2024-08-15 11:13:18 +0200

    chore(documentation): Update documentation version to 3.0.2
    Update the version used in the documentation and examples to 3.0.2 after
    the latest release.

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -8,7 +8,7 @@ data "azurerm_resource_group" "tstate" {
 
 module "terraform_state_storage_account" {
   source  = "ultratendency/secure-storage-account/azurerm"
-  version = "3.0.1"
+  version = "3.0.2"
 
   storage_account_name                = "tstate"
   storage_account_resource_group_name = data.azurerm_resource_group.tstate.name
